766177a90e9543158146b672e8b39c2ed029c4f5
emany <viajero28417@innovaccion.mx>
2020-11-13 10:50:19 -0600

    Clase13Nov

---
+++ assets/data/comentarios.php
@@ -0,0 +1,21 @@
+<?php
+    include('conexion_registro.php');
+
+    $obj = new Conexion;
+
+    $nombre     = $_POST['conNombre'];
+    $correo     = $_POST['conCcorreo'];
+    $asunto     = $_POST['conAsunto'];
+    $mensaje    = $_POST['conMensaje'];
+
+    $res = $obj->registraComentario($nombre,$correo,$asunto,$mensaje);
+
+    if($res == 1){
+        $datosreg  = array('dato' => 'ok');
+    }else{
+        $datosreg  = array('dato' => 'no');
+
+    }
+    echo json_encode($datosreg, JSON_FORCE_OBJECT);
+
+?>
--- assets/data/conexionGlobal.php
+++ assets/data/conexionGlobal.php
@@ -2,7 +2,6 @@
 
 class ConexionGlobal{
     var $conn;
-
     function conectar(){
               $conn = null;
           try{
@@ -32,66 +31,48 @@ class ConexionGlobal{
 
             return $numRegistros;
         }
-        function registroUsuario($user, $pass, $nombre, $correo){
+        
+        function registroUsuario($user, $pass, $nombre, $correo, $direccion, $telefono){
             $con = $this->conectar();
-             $stmt = $con->prepare('INSERT INTO usuario (usuario, contrasena, nombre, correo, direccion, telefono) VALUES (:user, :pass, :nom, :mail)');
+             $stmt = $con->prepare('INSERT INTO usuario (usuario, contrasena, nombre, correo, direccion, telefono) VALUES (:user, :pass, :nom, :mail, :dir, :tel)');
              $rows = $stmt->execute(array(':user'=>$user,
                                              ':pass'=>$pass,
                                              ':nom'=>$nombre,
-                                             ':mail'=>$correo));
+                                             ':mail'=>$correo,
+                                             ':dir'=>$direccion,
+                                             ':tel'=>$telefono));
              if($rows == 1){
                  echo 'Inserción correcta';
              
              }
           }
-      }
-
-?><?php
-
-class ConexionGlobal{
-    var $conn;
-
-    function conectar(){
-              $conn = null;
-          try{
-              $conn = new PDO('mysql:host=localhost;dbname=aztecagame','root','');
-              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-              //echo 'Se estableció la conexión <br> <br>';
-
-          }catch(PDOException $e){
-                die(print_r('Error conectando a la base de datos:' . $e->getMessage()));
-          } 
-        return $conn;
+          function registraComentario($nombre, $correo, $asunto, $mensaje){
+            $con = $this->conectar();
+             $stmt = $con->prepare('INSERT INTO comentario (nombre, correo, asunto, mensaje) VALUES (:nom, :mail, :asu, :men)');
+             $rows = $stmt->execute(array(':nom'=>$nombre,
+                                             ':mail'=>$correo,
+                                            ':asu'=>$asunto,
+                                            ':men'=>$mensaje));
+             if($rows == 1){
+                 echo 'Inserción correcta';
+             }
+            }
         }
-      
-        function buscarUsuario($user, $pass){
+
+        function buscarProducto(){
             $con = $this->conectar(); //mandar llamar al metodo de conectar
 
-            $consulta = 'SELECT nombre
-                                    FROM usuario
-                                    WHERE usuario=:usuario
-                                    AND contrasena=:pass';
+            $consulta = 'SELECT * FROM producto';
 
             $stmt = $con->prepare($consulta);
-            $stmt->execute(array(':usuario'=>$user,':pass'=>$pass));
-            $registro = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $numRegistros = count($registro);
+            $stmt->execute();
+            $registros = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            return $numRegistros;
-        }
-        function registroUsuario($user, $pass, $nombre, $correo){
-            $con = $this->conectar();
-             $stmt = $con->prepare('INSERT INTO usuario (usuario, contrasena, nombre, correo, direccion, telefono) VALUES (:user, :pass, :nom, :mail)');
-             $rows = $stmt->execute(array(':user'=>$user,
-                                             ':pass'=>$pass,
-                                             ':nom'=>$nombre,
-                                             ':mail'=>$correo));
-             if($rows == 1){
-                 echo 'Inserción correcta';
-             
-             }
-          }
-      }
+            json_encode($registros, JSON_FORCE_OBJECT);
+
+            return $registros;
+
+    }
+}
 
 ?>
